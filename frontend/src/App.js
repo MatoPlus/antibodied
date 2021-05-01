f303a5068ec32cdccde73cb345095051733b6c78
jerome2l <55860345+J3r0m3L@users.noreply.github.com>
2021-05-01 12:35:03 -0700

    Everything but markers

---
+++ frontend/src/App.js
@@ -0,0 +1,137 @@
+import React, { useRef, useCallback } from 'react';
+import './App.css';
+import { GoogleMap, useLoadScript, Marker, InfoWindow } from "@react-google-maps/api";
+import "@reach/combobox/styles.css";
+import mapStyles from "./mapStyles";
+import antibodyLogo from "./Antibody.png";
+import usePlacesAutocomplete, { getGeocode, getLatLng } from "use-places-autocomplete";
+import { Combobox, ComboboxInput, ComboboxPopover, ComboboxList, ComboboxOption } from "@reach/combobox";
+import SearchIcon from '@material-ui/icons/Search';
+import ExploreIcon from '@material-ui/icons/Explore';
+
+
+const libraries = ["places"];
+const mapContainerStyle = {
+  width: '100vw',
+  height: '100vh',
+  margin: 0,
+};
+{/*100vw, 100vh*/}
+const center = {
+  lat: 43.653225,
+  lng: -79.383186,
+};
+
+const options = {
+  styles: mapStyles,
+  disableDefaultUI: true,
+  zoomControl: true,
+};
+
+function App() {
+  const {isLoaded, loadError} = useLoadScript({
+    googleMapsApiKey: 'AIzaSyChf5WkmIVMEVYF1QlnAKhWAqnFzCxzPnQ',
+    libraries,
+  });
+
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  const panTo = React.useCallback(({lat, lng}) => {
+    mapRef.current.panTo({lat, lng});
+    mapRef.current.setZoom(14);
+  }, []);
+
+  if (loadError) return "Error Loading Maps";
+  if (!isLoaded) return "Loading Maps";
+
+  return (
+    <div className="App">
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap');
+      </style>
+      <div className="app_title">
+        <img src={antibodyLogo} className='app_logo'/>
+        <h1>Antibodied</h1>
+      </div>
+
+      <Locate panTo={panTo}/>
+      <Search panTo={panTo}/>
+
+      <GoogleMap
+        id="map"
+        mapContainerStyle={mapContainerStyle}
+        zoom={8}
+        center={center}
+        options={options}
+        onLoad={onMapLoad}
+      >
+
+      </GoogleMap>
+    </div>
+  );
+}
+
+function Locate({ panTo }) {
+  return (
+    <button className='app_locateButton' onClick={() => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        panTo({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      },
+      () => null
+      );
+    }}>
+      <ExploreIcon className='app_locateIcon'/>
+    </button>
+  );
+}
+
+function Search({ panTo }) {
+  const { ready, value, suggestions: {status, data}, setValue, clearSuggestions, } = usePlacesAutocomplete({
+    requestOptions: {
+      location: { lat: () => 43.653225, lng: () => -79.383186 },
+      radius: 200 * 1000,
+    }
+  });
+
+  return (
+    <div className='app_search'>
+      <Combobox
+        onSelect={async (address) => {
+          setValue(address, false);
+          clearSuggestions();
+          try {
+            const results = await getGeocode({ address });
+            const { lat, lng } = await getLatLng(results[0]);
+            panTo({ lat, lng });
+          } catch(error) {
+            console.log("error!")
+          }
+          console.log(address);
+        }}
+      >
+        <ComboboxInput
+          value={value}
+          onChange={(e) => {setValue(e.target.value);}}
+          disabled={!ready}
+          placeholder="Enter an Address "
+        />
+        <SearchIcon className='app_searchLogo'/>
+        <ComboboxPopover>
+          <ComboboxList>
+            {status === "OK" && data.map(({id, description}) => (
+              <ComboboxOption key={id} value={description} />
+            ))}
+          </ComboboxList>
+        </ComboboxPopover>
+      </Combobox>
+    </div>
+  );
+}
+
+export default App;
