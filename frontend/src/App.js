cf0cd29765d9ea45530111bf17e96288af24efc3
Rickson Yang <rixin.yang.2001@gmail.com>
2021-05-01 17:40:01 -0400

    remove dup cities

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,38 @@
-import React, { useRef, useCallback, useState } from 'react';
-import './App.css';
-import { GoogleMap, useLoadScript, Marker, InfoWindow } from "@react-google-maps/api";
+import React, { useRef, useCallback, useState } from "react";
+import "./App.css";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
 import "@reach/combobox/styles.css";
 import mapStyles from "./mapStyles";
 import antibodyLogo from "./Antibody.png";
-import usePlacesAutocomplete, { getGeocode, getLatLng } from "use-places-autocomplete";
-import { Combobox, ComboboxInput, ComboboxPopover, ComboboxList, ComboboxOption } from "@reach/combobox";
-import SearchIcon from '@material-ui/icons/Search';
-import ExploreIcon from '@material-ui/icons/Explore';
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import SearchIcon from "@material-ui/icons/Search";
+import ExploreIcon from "@material-ui/icons/Explore";
 import markers from "./constant";
-
 
 const libraries = ["places"];
 const mapContainerStyle = {
-  width: '100vw',
-  height: '100vh',
+  width: "100vw",
+  height: "100vh",
   margin: 0,
 };
-{/*100vw, 100vh*/}
+{
+  /*100vw, 100vh*/
+}
 const center = {
   lat: 43.653225,
   lng: -79.383186,
@@ -32,8 +47,8 @@ const options = {
 function App() {
   const [selectedPlace, setSelectedPlace] = useState(null);
 
-  const {isLoaded, loadError} = useLoadScript({
-    googleMapsApiKey: 'AIzaSyChf5WkmIVMEVYF1QlnAKhWAqnFzCxzPnQ',
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "AIzaSyChf5WkmIVMEVYF1QlnAKhWAqnFzCxzPnQ",
     libraries,
   });
 
@@ -42,8 +57,8 @@ function App() {
     mapRef.current = map;
   }, []);
 
-  const panTo = React.useCallback(({lat, lng}) => {
-    mapRef.current.panTo({lat, lng});
+  const panTo = React.useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
     mapRef.current.setZoom(14);
   }, []);
 
@@ -53,15 +68,16 @@ function App() {
   return (
     <div className="App">
       <style>
-        @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap');
+        @import
+        url('https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap');
       </style>
       <div className="app_title">
-        <img src={antibodyLogo} className='app_logo'/>
+        <img src={antibodyLogo} className="app_logo" />
         <h1>Antibodied</h1>
       </div>
 
-      <Locate panTo={panTo}/>
-      <Search panTo={panTo}/>
+      <Locate panTo={panTo} />
+      <Search panTo={panTo} />
 
       <GoogleMap
         id="map"
@@ -71,31 +87,32 @@ function App() {
         options={options}
         onLoad={onMapLoad}
       >
-
-      {markers.map((place) => (
-        <Marker
-          key={place.name}
-          position={{lat: place.latitude, lng: place.longitude }}
-          onClick={() => {
-            setSelectedPlace(place);
-          }}
-        />
-      ))}
-      {/*Grab all backend data and place within these div tags within the IndoWindow!*/}
-      {selectedPlace && (
-        <InfoWindow
-          position={{lat: selectedPlace.latitude, lng: selectedPlace.longitude }}
-          onCloseClick={() => {
-            setSelectedPlace(null)
-          }}
-        >
-          <div>
-            <h2>{selectedPlace.name}</h2>
-            <h2>thisworkslol</h2>
-          </div>
-        </InfoWindow>
-      )}
-
+        {markers.map((place) => (
+          <Marker
+            key={place.name}
+            position={{ lat: place.latitude, lng: place.longitude }}
+            onClick={() => {
+              setSelectedPlace(place);
+            }}
+          />
+        ))}
+        {/*Grab all backend data and place within these div tags within the IndoWindow!*/}
+        {selectedPlace && (
+          <InfoWindow
+            position={{
+              lat: selectedPlace.latitude,
+              lng: selectedPlace.longitude,
+            }}
+            onCloseClick={() => {
+              setSelectedPlace(null);
+            }}
+          >
+            <div>
+              <h2>{selectedPlace.name}</h2>
+              <h2>thisworkslol</h2>
+            </div>
+          </InfoWindow>
+        )}
       </GoogleMap>
     </div>
   );
@@ -103,31 +120,41 @@ function App() {
 
 function Locate({ panTo }) {
   return (
-    <button className='app_locateButton' onClick={() => {
-      navigator.geolocation.getCurrentPosition((position) => {
-        panTo({
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        });
-      },
-      () => null
-      );
-    }}>
-      <ExploreIcon className='app_locateIcon'/>
+    <button
+      className="app_locateButton"
+      onClick={() => {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            panTo({
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            });
+          },
+          () => null
+        );
+      }}
+    >
+      <ExploreIcon className="app_locateIcon" />
     </button>
   );
 }
 
 function Search({ panTo }) {
-  const { ready, value, suggestions: {status, data}, setValue, clearSuggestions, } = usePlacesAutocomplete({
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
     requestOptions: {
       location: { lat: () => 43.653225, lng: () => -79.383186 },
       radius: 200 * 1000,
-    }
+    },
   });
 
   return (
-    <div className='app_search'>
+    <div className="app_search">
       <Combobox
         onSelect={async (address) => {
           setValue(address, false);
@@ -136,24 +163,27 @@ function Search({ panTo }) {
             const results = await getGeocode({ address });
             const { lat, lng } = await getLatLng(results[0]);
             panTo({ lat, lng });
-          } catch(error) {
-            console.log("error!")
+          } catch (error) {
+            console.log("error!");
           }
           console.log(address);
         }}
       >
         <ComboboxInput
           value={value}
-          onChange={(e) => {setValue(e.target.value);}}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
           disabled={!ready}
           placeholder="Enter an Address "
         />
-        <SearchIcon className='app_searchLogo'/>
+        <SearchIcon className="app_searchLogo" />
         <ComboboxPopover>
           <ComboboxList>
-            {status === "OK" && data.map(({id, description}) => (
-              <ComboboxOption key={id} value={description} />
-            ))}
+            {status === "OK" &&
+              data.map(({ id, description }) => (
+                <ComboboxOption key={id} value={description} />
+              ))}
           </ComboboxList>
         </ComboboxPopover>
       </Combobox>
