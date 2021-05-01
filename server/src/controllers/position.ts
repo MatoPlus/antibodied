581cb677d2d535e157fdb3cb03830e19e8b4f79c
Rickson Yang <rixin.yang.2001@gmail.com>
2021-05-01 12:28:05 -0400

    improved filter, delegate sentiment to scrapper

--- server/src/controllers/position.ts
+++ server/src/controllers/position.ts
@@ -1,36 +1,25 @@
 import { Request, Response } from "express";
 import axios from "axios";
-import language from "@google-cloud/language";
-import { resolvePtr } from "node:dns";
 
 export const getPosts = async (req: Request, res: Response) => {
-  const client = new language.LanguageServiceClient();
-
   axios
     .get(process.env.GOOGLE_STORAGE_URL + req.params.id + ".json")
     .then(async (response) => {
-      const documents: any[] = [];
+      const positiveDocuments: any[] = [];
+      const negativeDocuments: any[] = [];
 
       for (const post of response.data.posts) {
-        const document = {
-          content: post.content as string,
-          type: "PLAIN_TEXT",
-        };
-
-        // Detects the sentiment of the text
-        const [result] = await client.analyzeSentiment({
-          document: document as any,
-        });
-        const sentiment = result.documentSentiment;
-
-        documents.push({
-          user: post.user,
-          content: post.content,
-          isPositive: sentiment?.score && sentiment.score >= 0 ? true : false,
-        });
+        if (post.isPositive) {
+          positiveDocuments.push(post);
+        } else {
+          negativeDocuments.push(post);
+        }
       }
 
-      return res.json({ posts: documents });
+      return res.json({
+        goodPosts: positiveDocuments,
+        badPosts: negativeDocuments,
+      });
     })
     .catch((err) => {
       console.log(err);
