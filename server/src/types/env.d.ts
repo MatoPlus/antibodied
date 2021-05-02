7cab9bf6474868da55c8985e0ce89ce9058affbf
Rickson Yang <rixin.yang.2001@gmail.com>
2021-05-02 14:09:00 -0400

    remove unnecessary env for server
    nlp model api call is done in scrapper

--- server/src/types/env.d.ts
+++ server/src/types/env.d.ts
@@ -2,7 +2,6 @@ declare namespace NodeJS {
   interface ProcessEnv {
     PORT: string;
     CORS_ORIGIN: string;
-    GOOGLE_APPLICATION_CREDENTIALS: string;
     GOOGLE_STORAGE_URL: string;
   }
 }
